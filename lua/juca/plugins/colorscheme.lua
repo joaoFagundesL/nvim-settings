b94c00939b43d7602a98068e7f96cc7cdc79ee58
Joao Fagundes <joao.230702@alunos.utfpr.edu.br>
2024-09-08 21:32:46 -0300

    att

--- lua/juca/plugins/colorscheme.lua
+++ lua/juca/plugins/colorscheme.lua
@@ -1,10 +1,12 @@
 return {
-  {
-    "ellisonleao/gruvbox.nvim",
-    priority = 1000, -- make sure to load this before all the other start plugins
+	{
+		-- "ellisonleao/gruvbox.nvim",
+		-- "folke/tokyonight.nvim",
+		"fxn/vim-monochrome",
+		priority = 1000, -- make sure to load this before all the other start plugins
 
-    config = function()
-      vim.cmd([[colorscheme gruvbox]])
-    end,
-  },
+		config = function()
+			vim.cmd([[colorscheme monochrome]])
+		end,
+	},
 }
