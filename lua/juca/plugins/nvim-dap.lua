d97e8db3f860a740c987ec3aa1e6904843ff6ea3
Joao Fagundes <joao.230702@alunos.utfpr.edu.br>
2025-03-08 17:45:59 -0300

    nvim-dap settings
    added an ui to nvim-dap

---
+++ lua/juca/plugins/nvim-dap.lua
@@ -0,0 +1,15 @@
+return {
+	"mfussenegger/nvim-dap",
+	event = "VeryLazy",
+	dependencies = {
+		"rcarriga/nvim-dap-ui",
+		"nvim-neotest/nvim-nio",
+		"jay-babu/mason-nvim-dap.nvim",
+		"theHamsta/nvim-dap-virtual-text",
+	},
+
+	config = function()
+		local dap, dapui = require("dap"), require("dapui")
+		dapui.setup()
+	end,
+}
